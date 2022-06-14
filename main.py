d2c404870c99f0c2bda8adb5a959821815ecc9d3
elizabethrsotomayor <elizabethrsotomayor@gmail.com>
2022-06-14 18:31:15 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def other_angle(a: int, b: int) -> int:
+    """
+    Return the third interior angle (in degrees) of a triangle.
+    :param a:
+    :param b:
+    :return:
+    """
+    return 180 - (a + b)
